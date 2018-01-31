f044fdd20ac45d2b2b63e71aa9a7b6e9e4f19754
Nick <Nick@NickMSI>
2018-01-30 22:14:40 -0500

    Template

---
+++ src/woottoni/Template.java
@@ -0,0 +1,23 @@
+//Name: Nick Wootton
+//What is?: 
+//Version: V1.0
+//Date Created: 
+//Date Last Edited: 
+
+package woottoni;
+
+//import
+
+/**
+ *
+ * @author Nick
+ */
+public class Template {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+}
